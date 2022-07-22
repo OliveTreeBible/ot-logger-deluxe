39dc889ec1f11c7ccf636245632b43ed4d9ab7c6
David Trotz <davidtrotz@olivetree.com>
2022-07-22 08:19:31 -0700

    Exporting LogLevel from typescript-logging

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export * from "./services/slack/SlackWebhook";
 export * from "./services/slack/SlackBodies";
 export * from "./LoggerDeluxe";
 export * from "./OTLogableMessage";
+export { LogLevel } from "typescript-logging"
